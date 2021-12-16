36bd10cdbbcd3eeacb06a0c33d3b3ac258283ba3
Matthew Shvorin <71457948+mshvorin@users.noreply.github.com>
2021-12-16 02:52:10 +0000

    limited to 21 numbers rather than 22

--- final.cpp
+++ final.cpp
@@ -10,7 +10,7 @@ void isrighttriangle(int s1,int s2,int s3){
 }
 
 void fizzbuzz(int integer){
-  for (integer =1; integer<23; ++integer){
+  for (integer =1; integer<22; ++integer){
     std::cout << integer;
   if (integer % 15 == 0){
     std::cout << " fizzbuzz\n";
@@ -26,7 +26,6 @@ void fizzbuzz(int integer){
 
 int main(){
   isrighttriangle(3,4,5);
-  fizzbuzz(30);
-  fizzbuzz(100);
+  fizzbuzz(22);
   return 0;
 }
